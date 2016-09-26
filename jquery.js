caf66789015cd221b7bd99f98fad41f89278162d
Rohan Hadnum <rhadnum@gmail.com>
2016-09-26 10:25:38 +0100

    added scoring system

--- jquery.js
+++ jquery.js
@@ -1,14 +1,28 @@
 $(document).ready(function(){
 
+	var score = 0;
+	var scoreValue = "#score";
 
 	$("li").click(function(){
 
 		if( $(this).hasClass("correct")){
-			alert("correct!")
+
+			$(this).css("background-color", "green")
+			$(this).css("color","white")
+			$("li").removeClass("incorrect")
+			score = score + 1;
+
+			$('#score').html(score)
+			
+		}
+
+		else if( $(this).hasClass("incorrect")){
+
+			$(this).css("background-color", "red")
+			$(this).css("color","white")
 		}
 
 	})
-
 
 
 })
